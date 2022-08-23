5e9e116cf82948d37321df9b574ab5ce8c0a6055
ArthurBukovecki <arthurbuk@gmail.com>
2022-08-24 00:50:55 +0200

    Test

--- Pages/StockData.py
+++ Pages/StockData.py
@@ -63,14 +63,45 @@ for ticker in best_performers['Ticker']:
         moving_averages = [150, 200]
         for ma in moving_averages:
             # window defines timeframe (e.g. 150 dats, 200 days, etc.)
-            df['SMA_' + str(ma)] = round(df['Ads Close'].rolling(window=ma).mean(), 2)
+            df['SMA_' + str(ma)] = round(df['Ads Close'].rolling(window=ma).mean(), 2) # rounded to 2 decimal places
 
         latest_price = df['Adj Close'][-1]
         # check documentation + function description for adding other statistics
-        pe_ratio = float(si.get_quote_data(ticker)['PE Ratio'])
+        # check stock.info() OR print(si.get_quote_data(ticker)) to see a DICT and choose
+        # some other field for more information
+        pe_ratio = float(si.get_quote_data(ticker)['PE Ratio (TTM)'])
+        # accessing individual fields
+        peg_ratio = float(si.get_stats_valuation(ticker)[1][4])
+        moving_average_150 = df('SMA_150')[-1]
+        moving_average_200 = df('SMA_200')[-1]
+        low_52week = round(min(df['Low'][-(52*5):]), 2)
+        high_52week = round(max(df['Low'][-(52*5):]), 2)
+        score = round(best_performers[best_performers['Ticker'] == ticker]['Score'].tolist())
 
-    except:
-        pass
+        # conditions for picking the stock
+
+        condition_1 = latest_price > moving_average_150 > moving_average_200
+        condition_2 = latest_price >= (1.3 * low_52week)
+        condition_3 = latest_price >= (0.75 * high_52week)
+        condition_4 = pe_ratio < 40
+        condition_5 = peg_ratio < 2
+
+        if condition_1 and condition_2 and condition_3 and condition_4 and condition_5:
+            final_df = final_df.append({'Ticker': ticker,
+                                        'Latest_Price': latest_price,
+                                        'Score': score,
+                                        'PE_Ratio': pe_ratio,
+                                        'SMA_150': moving_average_150,
+                                        'SMA_200': moving_average_200,
+                                        '52_Week_Low': low_52week,
+                                        '52_Week_High': high_52week}, ignore_index=True)
+    except Exception as e:
+        print(f"{e} for {ticker}")
+
+final_df.sort_values(by='Score', ascending=False)
+pd.set_option('display.max_columns', 10)
+print(final_df)
+final_df.to_csv()
 
 
 
